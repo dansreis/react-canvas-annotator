3c087fc1cf221915d33a47cf9258a408ae7bd804
Daniel Reis <danielreis3195@gmail.com>
2024-03-28 12:44:13 +0000

    Going back to styled components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import React, { MouseEventHandler } from "react";
+import styled from "styled-components";
 
 export type ButtonProps = {
   text?: string;
@@ -8,6 +9,25 @@ export type ButtonProps = {
   onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
+const StyledButton = styled.button<ButtonProps>`
+  border: 0;
+  line-height: 1;
+  font-size: 15px;
+  cursor: pointer;
+  font-weight: 700;
+  font-weight: bold;
+  border-radius: 10px;
+  display: inline-block;
+  color: ${(props) => (props.primary ? "#fff" : "#000")};
+  background-color: ${(props) => (props.primary ? "#FF5655" : "#f4c4c4")};
+  padding: ${(props) =>
+    props.size === "small"
+      ? "7px 25px 8px"
+      : props.size === "medium"
+        ? "9px 30px 11px"
+        : "14px 30px 16px"};
+`;
+
 const Button: React.FC<ButtonProps> = ({
   size,
   primary,
@@ -16,32 +36,17 @@ const Button: React.FC<ButtonProps> = ({
   onClick,
   ...props
 }) => {
-  // Determine button color and background color based on primary prop
-  const buttonColor = primary ? "text-white" : "text-black";
-  const bgColor = primary ? "bg-blue-500" : "bg-green-500";
-
-  // Determine padding based on size prop
-  let paddingClass = "";
-  if (size === "small") {
-    paddingClass = "py-1 px-6";
-  } else if (size === "medium") {
-    paddingClass = "py-2 px-8";
-  } else {
-    paddingClass = "py-3 px-8";
-  }
-
   return (
-    <>
-      <button
-        type="button"
-        onClick={onClick}
-        disabled={disabled}
-        className={`border-0 font-semibold rounded-lg inline-block cursor-pointer ${buttonColor} ${bgColor} ${paddingClass}`}
-        {...props}
-      >
-        {text}
-      </button>
-    </>
+    <StyledButton
+      type="button"
+      onClick={onClick}
+      primary={primary}
+      disabled={disabled}
+      size={size}
+      {...props}
+    >
+      {text}
+    </StyledButton>
   );
 };
 
