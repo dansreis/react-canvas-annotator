e16b249fa182a2b190e0463f39e23914b83a7b9b
Daniel Reis <danielreis3195@gmail.com>
2024-04-02 10:43:44 +0100

    Board component - Fixed tests error

--- src/components/Header/__test__/Header.test.tsx
+++ src/components/Header/__test__/Header.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
+import "@testing-library/jest-dom"; // This needs to be here for now.
 
 describe("Header component", () => {
   it("Header should render correctly", () => {
--- src/components/ToolbarItem/__test__/ToolbarItem.test.tsx
+++ src/components/ToolbarItem/__test__/ToolbarItem.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import ToolbarItem from "../ToolbarItem";
+import "@testing-library/jest-dom"; // This needs to be here for now.
 
 describe("ToolbarItem component", () => {
   it("ToolbarItem should render correctly", () => {
