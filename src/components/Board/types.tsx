97aec7b5e77db377529a5677ab3498c4628e722d
Daniel Reis <danielreis3195@gmail.com>
2024-06-28 12:19:30 +0100

    Added cursor list to allow custom cursors on polygons

--- src/components/Board/types.tsx
+++ src/components/Board/types.tsx
@@ -1,9 +1,13 @@
+import { CursorType } from "../../fabric/types";
+
 export type CanvasObject = {
   id: string;
   category: string;
   color: string;
   value: string;
   selectable?: boolean;
+  hoverCursor?: CursorType;
+  moveCursor?: CursorType;
   coords: { x: number; y: number }[];
   opacity?: number;
   content?: string;
